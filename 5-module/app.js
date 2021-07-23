9979d406302d40f616ea4b3f03dac5e0e7a4589d
Taeyang Kim <mynamesunpower@gmail.com>
2021-07-23 15:53:57 +0900

    modules

---
+++ 5-module/app.js
@@ -0,0 +1,8 @@
+import * as Counter from "./counter.js";
+// import {increase, getCount} from "./counter.js";
+
+Counter.increase();
+Counter.increase();
+Counter.increase();
+
+console.log(Counter.getCount())
